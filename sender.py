6b740d2857c6e189a1a2834b6ec0d1e5f5ca3630
Carlo Sumabat <jasumabat@up.edu.ph>
2022-05-31 03:16:41 +0800

    implemented command line parsing

--- sender.py
+++ sender.py
@@ -1,31 +1,49 @@
+from argparse import ArgumentParser
+from pprint import pprint
 import socket
 
 CLIENT_HOSTNAME = socket.gethostbyname(socket.gethostname())
-SERVER_HOSTNAME = "10.0.7.141"
-UDP_PORT_LISTEN = 6750        # listening port for UDP
-UDP_PORT_SEND = 9000          # sending port for UDP
-ID = "2b97c5ee"
-FILE = "2b97c5ee.txt"
+FILE = ""
+SERVER_HOSTNAME = ""
+UDP_PORT_SEND = 0  # sending port for UDP
+UDP_PORT_LISTEN = 0  # listening port for UDP
+ID = ""
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+def begin_transaction():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
 
-    # binding receiving sockets
-    udp_socket.bind((CLIENT_HOSTNAME, UDP_PORT_LISTEN))
+        # binding receiving sockets
+        udp_socket.bind((CLIENT_HOSTNAME, UDP_PORT_LISTEN))
 
-    # begin game
+        intent_msg = f"ID{ID}"
+        print(intent_msg)
+        print(CLIENT_HOSTNAME)
+        udp_socket.sendto(intent_msg.encode(), (SERVER_HOSTNAME, UDP_PORT_SEND))
 
-    intent_msg = f"ID{ID}"
-    print(intent_msg)
-    print(CLIENT_HOSTNAME)
-    udp_socket.sendto(intent_msg.encode(), (SERVER_HOSTNAME, UDP_PORT_SEND))
-    
-
-
-        
-    # receive results of round
-    data, addr = udp_socket.recvfrom(2048)
-    print(data.decode(), end="\n\n")
+        # receive results of round
+        data, addr = udp_socket.recvfrom(2048)
+        print(data.decode(), end="\n\n")
 
 
-        
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-f", help="filename/path of payload")
+    parser.add_argument("-a", help="IP address of receiver")
+    parser.add_argument("-s", help="port used by the receiver", type=int)
+    parser.add_argument("-c", help="port used by the sender", type=int)
+    parser.add_argument("-i", help="unique ID per student")
+    args = parser.parse_args()
+
+    FILE = args.f
+    SERVER_HOSTNAME = args.a
+    UDP_PORT_SEND = args.s
+    UDP_PORT_LISTEN = args.c
+    ID = args.i
+
+    print(FILE)
+    print(SERVER_HOSTNAME)
+    print(UDP_PORT_SEND)
+    print(UDP_PORT_LISTEN)
+    print(ID)
+    # begin_transaction()
