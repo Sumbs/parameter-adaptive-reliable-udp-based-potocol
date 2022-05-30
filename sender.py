9cb85f39f69188cdb156adff4a5c4e5b6ec228c0
Carlo Sumabat <jasumabat@up.edu.ph>
2022-05-31 03:17:05 +0800

    implemented command line parsing

--- sender.py
+++ sender.py
@@ -2,8 +2,9 @@ from argparse import ArgumentParser
 from pprint import pprint
 import socket
 
+# program parameters
 CLIENT_HOSTNAME = socket.gethostbyname(socket.gethostname())
-FILE = ""
+FILE = ""  # file to be sent by client
 SERVER_HOSTNAME = ""
 UDP_PORT_SEND = 0  # sending port for UDP
 UDP_PORT_LISTEN = 0  # listening port for UDP
@@ -13,12 +14,12 @@ ID = ""
 def begin_transaction():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
 
-        # binding receiving sockets
+        # bind receiving socket
         udp_socket.bind((CLIENT_HOSTNAME, UDP_PORT_LISTEN))
 
+        # send intent message
         intent_msg = f"ID{ID}"
-        print(intent_msg)
-        print(CLIENT_HOSTNAME)
+        print(f"intent message: {intent_msg}")
         udp_socket.sendto(intent_msg.encode(), (SERVER_HOSTNAME, UDP_PORT_SEND))
 
         # receive results of round
@@ -27,6 +28,7 @@ def begin_transaction():
 
 
 if __name__ == "__main__":
+    # parse command line arguments
     parser = ArgumentParser()
     parser.add_argument("-f", help="filename/path of payload")
     parser.add_argument("-a", help="IP address of receiver")
@@ -35,15 +37,16 @@ if __name__ == "__main__":
     parser.add_argument("-i", help="unique ID per student")
     args = parser.parse_args()
 
+    # initializing global variables
     FILE = args.f
     SERVER_HOSTNAME = args.a
     UDP_PORT_SEND = args.s
     UDP_PORT_LISTEN = args.c
     ID = args.i
 
-    print(FILE)
-    print(SERVER_HOSTNAME)
-    print(UDP_PORT_SEND)
-    print(UDP_PORT_LISTEN)
-    print(ID)
-    # begin_transaction()
+    print(f"File to send: {FILE}")
+    print(f"IP address of server: {SERVER_HOSTNAME}")
+    print(f"Port used by server: {UDP_PORT_SEND}")
+    print(f"Port used by client: {UDP_PORT_LISTEN}")
+    print(f"Unique ID: {ID}")
+    begin_transaction()
